355d584ef8a53ffed418bd74c24d577e25d4ada9
Daniel <daniel.reis1911@gmail.com>
2025-04-19 16:08:47 -0300

    Start working with files.

--- app.c
+++ app.c
@@ -5,10 +5,25 @@
 #define MAX 549
 
 int main(void) {
+	int i;
 	char title[MAX][250], author[MAX][250];
-	float price;
+	float price[MAX];
 	
+	FILE *stream;
+	fopen("data.csv", "r");
 	
+	for(i = 0; i < MAX; i++) {
+		fscanf(stream, "%s", &title[i]);
+		fscanf(stream, "%s", &author[i]);
+		fscanf(stream, "%f", &price[i]);
+	}
 	
+	for(i = 0; i < MAX; i++) {
+		printf("%s", title[i]);
+		printf("%s", author[i]);
+		printf("%f", price[i]);
+	}
+	
+	fclose(stream);
 	return 0;
 }
